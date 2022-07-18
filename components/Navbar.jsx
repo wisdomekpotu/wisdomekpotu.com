981ef75e24044f70141ec5b7ed42a3477922172d
wisdom ekpotu <whizfidel@gmail.com>
2022-07-18 18:41:57 +0100

    modifeid links tag

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -62,25 +62,25 @@ export default function Navbar() {
 
           <ul className='DESKTOP-MENU hidden space-x-9 lg:flex'>
             <li className=' text-emerald-400'>
-              <a href='/blog'>
+              <Link href='/blog'>
                 {' '}
                 <FontAwesomeIcon
                   icon={faPen}
                   style={{ fontSize: 14, color: '#01D5A2' }}
                 />{' '}
                 Blog
-              </a>
+              </Link>
             </li>
 
             <li className=' text-emerald-400 '>
-              <a href='/projects'>
+              <Link href='/projects'>
                 {' '}
                 <FontAwesomeIcon
                   icon={faCodeBranch}
                   style={{ fontSize: 14, color: '#01D5A2' }}
                 />{' '}
                 Projects
-              </a>
+              </Link>
             </li>
           </ul>
         </nav>
