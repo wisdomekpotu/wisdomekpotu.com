ab876b7f4916f18601b24d5692480fc7c45ce52b
wisdom ekpotu <whizfidel@gmail.com>
2022-07-29 16:51:52 +0100

    rebuilt blog page

--- components/Article/Card.jsx
+++ components/Article/Card.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import Firebase from '../../public/asset/firebase.webp';
+import Mobile from '../../public/asset/mobile.webp';
+import MDX from '../../public/asset/MDX.webp';
 
 export default function Card({
   title,
@@ -17,7 +20,75 @@ export default function Card({
         <div className='container px-5 py-24 mx-auto'>
           <div className='flex flex-wrap -m-4'>
             <div className='p-4 md:w-1/3'>
-              <div className='h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden'>
+              <a
+                href='https://blog.openreplay.com/authentication-in-react-18-using-firebase-v9'
+                className='block overflow-hidden rounded-2xl'
+              >
+                <Image
+                  className='object-cover w-full h-56'
+                  src={Firebase}
+                  alt=''
+                />
+
+                <div className='p-4 bg-gray-900'>
+                  <p className='text-xs text-gray-500'>blog.openreplay.com</p>
+
+                  <h5 className='text-sm text-white'>
+                    Authentication in React 18 using Firebase v9
+                  </h5>
+
+                  <p className='mt-1 text-xs text-gray-500'>
+                    Implement an authentication flow using Firebase, for any
+                    React app
+                  </p>
+                </div>
+              </a>
+            </div>
+            <div className='p-4 md:w-1/3'>
+              <a
+                href='https://blog.openreplay.com/building-a-mobile-app-using-html-css-and-javascript'
+                className='block overflow-hidden rounded-2xl'
+              >
+                <Image
+                  className='object-cover w-full h-56'
+                  src={Mobile}
+                  alt=''
+                />
+
+                <div className='p-4 bg-gray-900'>
+                  <p className='text-xs text-gray-500'>blog.openreplay.com</p>
+
+                  <h5 className='text-sm text-white'>
+                    Building a Mobile App using HTML, CSS, and JavaScript
+                  </h5>
+
+                  <p className='mt-1 text-xs text-gray-500'>
+                    A back-to-basics way of developing a mobile app
+                  </p>
+                </div>
+              </a>
+            </div>
+            <div className='p-4 md:w-1/3'>
+              <a
+                href='https://blog.openreplay.com/build-a-mdx-powered-blog-with-contentlayer-and-next'
+                className='block overflow-hidden rounded-2xl'
+              >
+                <Image className='object-cover w-full h-56' src={MDX} alt='' />
+
+                <div className='p-4 bg-gray-900'>
+                  <p className='text-xs text-gray-500'>blog.openreplay.com</p>
+
+                  <h5 className='text-sm text-white'>
+                    Build a MDX-powered blog with Contentlayer and Next
+                  </h5>
+
+                  <p className='mt-1 text-xs text-gray-500'>
+                    A simple way of creating a static Markdown-based blog site
+                  </p>
+                </div>
+              </a>
+            </div>
+            {/* <div className='h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden'>
                 <Image
                   className='lg:h-48 md:h-36 w-full object-cover object-center'
                   src={image}
@@ -72,8 +143,7 @@ export default function Card({
                     </span>
                   </div>
                 </div>
-              </div>
-            </div>
+              </div> */}
           </div>
         </div>
       </section>
