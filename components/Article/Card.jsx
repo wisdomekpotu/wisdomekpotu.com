70a94c402679b9af21563735a6635674b1945546
Wisdom Ekpotu <whizfidel@gmail.com>
2024-01-15 12:32:27 +0000

    fixed image format

--- components/Article/Card.jsx
+++ components/Article/Card.jsx
@@ -11,7 +11,7 @@ import Uselayout from "../../public/asset/Uselayout.webp"
 import Palmapi from "../../public/asset/Palmapi.webp"
 import Hack from '../../public/asset/Hack.png';
 import Network from '../../public/asset/Network.png';
-import Native from '../../public/asset/Native.avif'
+import Native from '../../public/asset/Native.png'
 
 export default function Card({
   title,
